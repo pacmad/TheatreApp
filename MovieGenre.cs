f960fead0cddeccd18c291aa4b55c3870f3e111a
Sneha Santhosh <45783979+Sneha-Santhosh@users.noreply.github.com>
2019-11-08 23:52:58 -0500

    Update MovieGenre.cs

--- MovieGenre.cs
+++ MovieGenre.cs
@@ -1,7 +1,19 @@
 ﻿using System;
 namespace TheatreApp
 {
+    [Flags]
     public enum MovieGenre
     {
-    }
+     Unrated =0,
+     Action=1,
+     Romance = 2,
+     Documentary =4,
+     Mystery = 8,
+     Musical =16,
+     Horror = 32,
+     Comedy = 64,
+     Fantasy = 128,
+     Animation = 256,
+     Adventure = 512
+    };
 }
